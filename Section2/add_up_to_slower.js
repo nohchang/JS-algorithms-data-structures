fa82cc4b831d5da9640b57967eaef17e92f25e7b
nohchang <nchq2123@naver.com>
2022-12-07 21:55:25 +0900

    section2

---
+++ Section2/add_up_to_slower.js
@@ -0,0 +1,12 @@
+function addUpTo(n) {
+	let total = 0
+	for (let i = 1; i <= n; i++) {
+		total += i
+	}
+	return total
+}
+
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
