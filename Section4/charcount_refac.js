b9cc8901cf09efbc703b5fa8694badfafce65c41
nohchang <nchq2123@naver.com>
2022-12-14 20:32:39 +0900

    section4

---
+++ Section4/charcount_refac.js
@@ -0,0 +1,74 @@
+// 1
+
+// function charCount(str) {
+//   var obj = {}
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str[i].toLowerCase()
+//     if (/[a-z0-9]/.test(char)) {
+//       if (obj[char] > 0) {
+//         obj[char]++
+//       } else {
+//         obj[char] = 1
+//       }
+//     }
+//   }
+//   return obj
+// }
+
+
+// 2
+
+// function charCount(str) {
+//   var obj = {}
+//   for (var char of str) {
+//     char = char.toLowerCase()
+//     if (/[a-z0-9]/.test(char)) {
+//       if (obj[char] > 0) {
+//         obj[char]++
+//       } else {
+//         obj[char] = 1
+//       }
+//     }
+//   }
+//   return obj
+// }
+
+
+// 3
+
+// function charCount(str) {
+//   var obj = {}
+//   for (var char of str) {
+//     char = char.toLowerCase()
+//     if (/[a-z0-9]/.test(char)) {
+//       obj[char] = ++obj[char] || 1
+//     }
+//   }
+//   return obj
+// }
+
+
+//4 
+
+function charCount(str) {
+  var obj = {}
+  for (var char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase()
+      obj[char] = ++obj[char] || 1
+    }
+  }
+  return obj
+}
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0)
+  if (!(code > 47 && code < 58) &&
+      !(code > 64 && code < 91) &&
+      !(code > 96 && code < 123)) {
+        return false
+      }
+      return true
+}
+
+console.log(charCount("Hello WORLD hi!!!!"))
