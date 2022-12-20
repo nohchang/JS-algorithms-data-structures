83b648fe66a79df6e2efaf5fb629a7491663ccea
nohchang <nchq2123@naver.com>
2022-12-20 18:58:39 +0900

    section12

---
+++ Section12/selection_sort.js
@@ -0,0 +1,40 @@
+// function selectionSort(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     var lowest =  i
+//     for (var j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[lowest]) {
+//         lowest = j
+//       }
+//     }
+//     if (i !== lowest) {
+//       console.log("**************")
+//       console.log(arr)
+//       console.log("SWAPPING TO: ")
+//       var temp = arr[i]
+//       arr[i] = arr[lowest]
+//       arr[lowest] = temp
+//       console.log(arr)
+//       console.log("**************")
+//     }
+//   }
+//   return arr
+// }
+
+// ES2015
+
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest)
+  }
+  return arr
+}
+
+console.log(selectionSort([0,2,34,22,10,19,17]))
