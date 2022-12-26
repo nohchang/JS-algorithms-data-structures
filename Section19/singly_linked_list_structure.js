27d628aa87b64fbd1dc88b6f42f416e453d242fc
nohchang <nchq2123@naver.com>
2022-12-26 19:11:19 +0900

    section19 -ing

---
+++ Section19/singly_linked_list_structure.js
@@ -0,0 +1,66 @@
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+  push(val) {
+    var newNode = new Node(val)
+    if (!this.head ) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+  pop() {
+    if (!this.head) return undefined
+    var current = this.head
+    var newTail = current
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return current
+  }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+// push
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("!")
+console.log(list)
+
+
+// pop
+console.log(list.pop())
+console.log(list)
+console.log(list.pop())
+console.log(list)
