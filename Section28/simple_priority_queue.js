bf2083b72fc1b1bfb3af37350ec484c1a5984c8f
nohchang <nchq2123@naver.com>
2023-01-11 19:43:20 +0900

    section28

---
+++ Section28/simple_priority_queue.js
@@ -0,0 +1,24 @@
+class PriorityQueue {
+  constructor() {
+    this.values = []
+  }
+  enqueue(val, priority) {
+    this.values.push({val, priority})
+    this.sort()
+  }
+  dequeue() {
+    return this.values.shift()
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority)
+  }
+}
+
+let q = new PriorityQueue()
+
+q.enqueue("B", 3)
+q.enqueue("C", 5)
+q.enqueue("D", 2)
+q.enqueue("Q", 20)
+
+console.log(q.values)
